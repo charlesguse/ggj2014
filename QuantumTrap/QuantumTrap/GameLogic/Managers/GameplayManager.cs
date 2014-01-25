5fa4c3b586ae654bd8a561f222d2d20e212756f6
Charlie Guse <charlesguse@users.noreply.github.com>
2014-01-25 14:28:04 -0500

    Added a lot of the managers and functions needed.

---
+++ QuantumTrap/QuantumTrap/GameLogic/Managers/GameplayManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using QuantumTrap.ScreenManagers;
+
+namespace QuantumTrap.GameLogic.Managers
+{
+    public class GameplayManager
+    {
+        public PlayerManager PlayerManager { get; set; }
+        public ShadowManager ShadowManager { get; set; }
+        public LevelManager LevelManager { get; set; }
+        public WinManager WinManager { get; set; }
+
+        public GameplayManager()
+        {
+            PlayerManager = new PlayerManager();
+            ShadowManager = new ShadowManager();
+            LevelManager = new LevelManager();
+            WinManager = new WinManager();
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            PlayerManager.Update(gameTime);
+            ShadowManager.Update(gameTime);
+            LevelManager.Update(gameTime);
+            WinManager.Update(PlayerManager, ShadowManager);
+        }
+
+        public void HandleInput(GameTime gameTime, InputState input)
+        {
+            PlayerManager.HandleInput(gameTime, input, LevelManager);
+            ShadowManager.HandleInput(gameTime, input, LevelManager);
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            LevelManager.Draw(gameTime, spriteBatch);
+            ShadowManager.Draw(gameTime, spriteBatch);
+            PlayerManager.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+    }
+}
