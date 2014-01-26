3cc17e6e39669a0c963eb1e2a4c0a2eeecb0a733
ryusage <blaquer@gmail.com>
2014-01-25 19:45:25 -0500

    Level importer might be working

--- QuantumTrap/QuantumTrap/LevelImporter.cs
+++ QuantumTrap/QuantumTrap/LevelImporter.cs
@@ -16,7 +16,44 @@ namespace QuantumTrap
             var filePath = BuildFileNameFromLevelName(levelName);
             var levelXml = LoadLevelXml(filePath);
 
+            var level = BuildLevelFromXml(levelXml);
+
             return new Level();
+        }
+
+        private Level BuildLevelFromXml(XmlDocument levelXml)
+        {
+            var level = new Level();
+
+            var player = levelXml.SelectSingleNode("level/Objects/Player");
+            var shadow = levelXml.SelectSingleNode("level/Objects/Shadow");
+            LoadTiles(level, levelXml, "RedTiles", TileType.Red);
+            LoadTiles(level, levelXml, "BlueTiles", TileType.Blue);
+            LoadTiles(level, levelXml, "GreenTiles", TileType.Green);
+            LoadTiles(level, levelXml, "YellowTiles", TileType.Yellow);
+            LoadTiles(level, levelXml, "BlackTiles", TileType.Black);
+
+            return level;
+        }
+
+        private void LoadTiles(Level level, XmlDocument levelXml, string tileNodeName, TileType tileType)
+        {
+            var tileNode = levelXml.SelectSingleNode("//" + tileNodeName);
+            var bitString = tileNode.InnerText;
+
+            for (int y = 0; y < Constants.LEVEL_HEIGHT; y++)
+            {
+                for (int x = 0; x < Constants.LEVEL_WIDTH; x++)
+                {
+                    var index = (y*Constants.LEVEL_WIDTH) + x + y; // the extra y being added is for newlines
+                    var bitValue = bitString[index];
+
+                    if (bitValue == '1')
+                    {
+                        level.TileMap[x][y].TileType = tileType;
+                    }
+                }
+            }
         }
 
         private XmlDocument LoadLevelXml(string filePath)
