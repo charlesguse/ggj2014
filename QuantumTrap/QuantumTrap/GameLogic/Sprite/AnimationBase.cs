0b6c2e9e8540e332308ddf2aa9317ca213caf766
Charlie Guse <charlesguse+sourcetree@gmail.com>
2014-01-26 12:44:39 -0500

    Added animation stuff in case we use it

---
+++ QuantumTrap/QuantumTrap/GameLogic/Sprite/AnimationBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace QuantumTrap.GameLogic.Sprite
+{
+    public abstract class AnimationBase
+    {
+        /// <summary>
+        /// All frames in the animation arranged horizontally.
+        /// </summary>
+        public Texture2D Texture { get; set; }
+
+        /// <summary>
+        /// Gets the size of a frame in the animation.
+        /// </summary>
+        public Rectangle Frame { get; protected set; }
+
+        /// <summary>
+        /// Gets the number of frames in the animation.
+        /// </summary>
+        public int FrameCount { get; protected set; }
+        
+        /// <summary>
+        /// Duration of time to show each frame.
+        /// </summary>
+        public float FrameTime { get; protected set; }
+
+        /// <summary>
+        /// When the end of the animation is reached, should it
+        /// continue playing from the beginning?
+        /// </summary>
+        public bool IsLooping { get; protected set; }
+    }
+}
