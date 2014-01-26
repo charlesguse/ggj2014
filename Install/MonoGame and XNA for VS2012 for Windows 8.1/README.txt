186b76b275dc73ca3b2d131e01d7550e603a8700
heydottim <hey.tim@gmail.com>
2014-01-26 14:03:51 -0500

    adding story levels 4,5 and end. Updated story level 2.

---
+++ Install/MonoGame and XNA for VS2012 for Windows 8.1/README.txt
@@ -0,0 +1,12 @@
+Before install extension:
+1. Install Games for Windows Live
+2. Install MonoGame
+   a) Run C:\Program Files (x86)\MonoGame\v3.0\oalinst.exe
+3. Install Windows Phone 7.1 SDK (Necessary to load content projects for any XNA game)
+4. Install DirectX
+5. Install XNA Framework 4.0 Redistribution
+6. Install XNA Game Studio Platform Tools
+7. Install XNA Game Studio 4.0 2012 Extension
+
+If you are getting an error "Error \PSM\IgnoreMe.dll could not be found"
+Open the solution properties, go to Configuration Properties and check the build option for the content project
---
+++ QuantumTrap/OgmoXNAPipelineExtensions/ContentItems/ObjectTemplateContent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using OgmoXNAPipelineExtensions.ContentItems.Values;
+
+namespace OgmoXNAPipelineExtensions.ContentItems
+{
+    public class ObjectTemplateContent
+    {
+        public int Height;
+        public bool IsResizableX;
+        public bool IsResizableY;
+        public bool IsTiled;
+        public string Name;
+        public Vector2 Origin;
+        public Rectangle Source;
+        public string TextureFile;
+        public ExternalReference<TextureContent> TextureReference;
+        public int Width;
+        public List<ValueTemplateContent> Values = new List<ValueTemplateContent>();
+
+        public ObjectTemplateContent()
+        {
+        }
+
+        public ObjectTemplateContent(XmlNode node)
+        {
+            if (node.Attributes["Height"] != null)
+                this.Height = int.Parse(node.Attributes["Height"].Value, CultureInfo.InvariantCulture);
+            if (node.Attributes["Width"] != null)
+                this.Width = int.Parse(node.Attributes["Width"].Value, CultureInfo.InvariantCulture);
+            if (node.Attributes["ResizableX"] != null)
+                this.IsResizableX = bool.Parse(node.Attributes["ResizableX"].Value);
+            if (node.Attributes["ResizableY"] != null)
+                this.IsResizableY = bool.Parse(node.Attributes["ResizableY"].Value);
+            if (node.Attributes["Tile"] != null)
+                this.IsTiled = bool.Parse(node.Attributes["Tile"].Value);
+            if (node.Attributes["Name"] != null)
+                this.Name = node.Attributes["Name"].Value;
+            if (node.Attributes["OriginX"] != null)
+                this.Origin.X = int.Parse(node.Attributes["OriginX"].Value, CultureInfo.InvariantCulture);
+            if (node.Attributes["OriginY"] != null)
+                this.Origin.Y = int.Parse(node.Attributes["OriginY"].Value, CultureInfo.InvariantCulture);
+            if (node.Attributes["ImageOffsetX"] != null)
+                this.Source.X = int.Parse(node.Attributes["ImageOffsetX"].Value, CultureInfo.InvariantCulture);
+            else
+                this.Source.X = 0;
+            if (node.Attributes["ImageOffsetY"] != null)
+                this.Source.Y = int.Parse(node.Attributes["ImageOffsetY"].Value, CultureInfo.InvariantCulture);
+            else
+                this.Source.Y = 0;
+            if (node.Attributes["ImageWidth"] != null)
+                this.Source.Width = int.Parse(node.Attributes["ImageWidth"].Value, CultureInfo.InvariantCulture);
+            else
+                this.Source.Width = this.Width;
+            if (node.Attributes["ImageHeight"] != null)
+                this.Source.Height = int.Parse(node.Attributes["ImageHeight"].Value, CultureInfo.InvariantCulture);
+            else
+                this.Source.Height = this.Height;
+            if (node.Attributes["Image"] != null)
+                this.TextureFile = node.Attributes["Image"].Value;
+            // Values
+            XmlNode valuesNode = node.SelectSingleNode("Values");
+            if (valuesNode != null)
+            {
+                foreach (XmlNode valueNode in valuesNode.SelectNodes("Boolean|Integer|Number|String|Text"))
+                {
+                    ValueTemplateContent valueContent = ValueContentTemplateParser.Parse(valueNode);
+                    if (valueContent != null)
+                        this.Values.Add(valueContent);
+                }
+            }
+        }
+    }
+}
