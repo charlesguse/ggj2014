f241aac5a223b8fe94f4ab31cb2a73448880054a
ryusage <blaquer@gmail.com>
2014-01-26 12:54:21 -0500

    Made a way overcomplicated shake animation when the player tries to move but cant

---
+++ QuantumTrap/QuantumTrap/GameLogic/Managers/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace QuantumTrap.GameLogic.Managers
+{
+    public class LevelManager
+    {
+        private readonly string _levelFile;
+        public Level Level { get; set; }
+
+        public LevelManager(string levelFile)
+        {
+            _levelFile = levelFile;
+            Level = new Level();
+        }
+
+        public void LoadContent(ContentManager content, PlayerManager player)
+        {
+            var importer = new LevelImporter();
+            Level = importer.Import(_levelFile);
+            Level.LoadContent(content);
+            player.Player.Position = Level.PlayerStart;
+            player.Player.SetDrawablePosition(player.Player.Position);
+            player.Shadow.Position = Level.ShadowStart;
+            player.Shadow.SetDrawablePosition(player.Shadow.Position);
+        }
+
+        public void LoadLevel(string levelName, PlayerManager playerManager)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Level.Update(gameTime);
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Player player, Shadow shadow)
+        {
+            Level.Draw(gameTime, spriteBatch, player, shadow);
+        }
+
+        internal bool CanMoveTo(Position2 potentialPostion, PlayerColor playerColor)
+        {
+            return Level.CanMoveTo(potentialPostion, playerColor);
+        }
+    }
+}
