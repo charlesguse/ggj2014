c3cb9eda2b3780ee254cefb3c1de1cbd43bfe175
Charlie Guse <charlesguse+sourcetree@gmail.com>
2014-01-26 14:46:21 -0500

    Changed the win condition to check a few frames

--- QuantumTrap/QuantumTrap/GameLogic/Managers/WinManager.cs
+++ QuantumTrap/QuantumTrap/GameLogic/Managers/WinManager.cs
@@ -2,11 +2,18 @@ namespace QuantumTrap.GameLogic.Managers
 {
     public class WinManager
     {
+        private int _framesTogether;
         public bool GameWon { get; set; }
 
         public void Update(PlayerManager playerManager)
         {
-            GameWon = playerManager.Player.Position == playerManager.Shadow.Position;
+            if (playerManager.Player.Position == playerManager.Shadow.Position)
+                _framesTogether++;
+            else
+                _framesTogether = 0;
+
+            if (_framesTogether >= 3)
+                GameWon = playerManager.Player.Position == playerManager.Shadow.Position;
         }
     }
 }
